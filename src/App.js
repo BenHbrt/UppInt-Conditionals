120b63ff2976045bd5468bfc588517e436c035de
BenHbrt <benherbert1983@googlemail.com>
2023-02-07 14:56:51 +0100

    Initial screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.scss';
+import { useState } from 'react'
+import initialScores from './data/InitialScores';
 
 function App() {
+
+  const [sectionScores, setSectionScores] = useState(initialScores)
   return (
     <div className="App">
-      <h1>Text</h1>
+      <h1>Upper-Intermediate Conditionals</h1>
+      <div className="menu">
+        {
+          sectionScores.map((item) => {
+            return <div className="menu_option">
+              <span>{item.conditional}</span>
+              {item.score === 0 ? <span>try</span> : <span>{item.score}</span>}
+              </div>
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/data/InitialScores.js
@@ -0,0 +1,24 @@
+const initialScores = [
+    {
+        conditional: "Zero Conditional",
+        score: 0
+    },
+    {
+        conditional: "First Conditional",
+        score: 0
+    },
+    {
+        conditional: "Second Conditional",
+        score: 0
+    },
+    {
+        conditional: "Third Conditional",
+        score: 0
+    },
+    {
+        conditional: "Mixed Conditionals",
+        score: 0
+    }
+]
+
+export default initialScores
